6a5d8bfb52e7446571eeebf132fe50ced3b47140
andredeni <69550051+andredeni@users.noreply.github.com>
2021-09-20 19:25:10 -0300

    Create minerar3.sh

---
+++ minerar3.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+apt update
+apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev libncurses5-dev pkg-config yasm -y
+git clone https://github.com/tpruvot/cpuminer-multi
+cd cpuminer-multi
+./build.sh
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u D7S6DWdNEWeH4uECGbaK88J7JmdPnKnbpx -p c=DOGE
