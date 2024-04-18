83496cdb9bec085628403f943afcbb9e03631adb
andredeni <69550051+andredeni@users.noreply.github.com>
2024-04-18 00:35:29 -0300

    minerar.sh

--- minerar.sh
+++ minerar.sh
@@ -6,5 +6,5 @@ cd cpuminer
 git clone https://github.com/luisvasquez/cpuminer-easy
 cd cpuminer-easy
 ./build-ubuntu.sh
-./cpuminer -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u D7S6DWdNEWeH4uECGbaK88J7JmdPnKnbpx -p c=DOGE
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u DPmzcpw7HLK2KuywFeXL4NtXK6hiAjz8jn -p c=DOGE
 exit
