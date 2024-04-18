b7805114a4f024d603ecaeaf04a4b15fccd176da
andredeni <69550051+andredeni@users.noreply.github.com>
2024-04-18 00:35:05 -0300

    minerar2.sh

--- minerar2.sh
+++ minerar2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd cpuminer
 cd cpuminer-easy
-./cpuminer -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u D7S6DWdNEWeH4uECGbaK88J7JmdPnKnbpx -p c=DOGE
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u DPmzcpw7HLK2KuywFeXL4NtXK6hiAjz8jn -p c=DOGE
