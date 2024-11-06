25eb4a061979e0f396dd3e59f2b79afd47d45a4d
andredeni <69550051+andredeni@users.noreply.github.com>
2024-11-06 14:23:42 -0300

    minerar4.sh

--- minerar4.sh
+++ minerar4.sh
@@ -1,5 +1,5 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 while [ 1 ]; do
-./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u DATVawzWxzUmPUUfyHYmVvmLpzTBg53HbG -p c=DOGE
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u DLWMwktsC6QT1ppqkdVPMvvG2Wr9NymkRq -p c=DOGE
 done
